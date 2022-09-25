d7aab254f5a90dcc551f0fa449b2f32660ae2a01
LitvinT <litvin.lara74mail.ru@gmail.com>
2022-09-25 22:44:05 +0300

    rewn

--- lesson 3 take 4.py
+++ lesson 3 take 4.py
@@ -1,4 +1,31 @@
-a = int(input('Введите 1 число: '))
-b = int(input('Введите 2 число: '))
-c = int(input('Введите 3 число: '))
-print( (a>0), (b>0), (c>0))
+#a = int(input('Введите 1 число: '))
+#b = int(input('Введите 2 число: '))
+#c = int(input('Введите 3 число: '))
+#print( (a>0), (b>0), (c>0))
+
+a = float(input('Введите число:'))
+if a >= 0:
+    if a == 0:
+        print('zero')
+    else:
+        print('Положительное')
+else:
+    print('отрицательное')
+
+b =float(input('Введите число:'))
+if b >= 0:
+    if b == 0:
+        print('zero')
+    else:
+        print('Положительное')
+else:
+    print('отрицательное')
+
+c = float(input('Введите число:'))
+if c >= 0:
+    if c == 0:
+        print('zero')
+    else:
+        print('Положительное')
+else:
+    print('отрицательное')
